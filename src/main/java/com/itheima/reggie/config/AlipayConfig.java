e210fd79c33c7f32e4701cbe92e75de7c56a85b8
fxh <1246654281@qq.com>
2024-03-24 13:02:53 +0800

    1

--- src/main/java/com/itheima/reggie/config/AlipayConfig.java
+++ src/main/java/com/itheima/reggie/config/AlipayConfig.java
@@ -1,15 +1,4 @@
-package com.itheima.reggie.config;/*
 package com.itheima.reggie.config;
-
-import com.alipay.easysdk.factory.Factory;
-import com.alipay.easysdk.kernel.Config;
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-
-*/
 
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
@@ -18,40 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @Author
- * @Date Created in  2023/5/5 15:06
- * @DESCRIPTION:
- * @Version V1.0
- *//*
 
-
-@Data
-@Component
-//读取yml文件中alipay 开头的配置
-@ConfigurationProperties(prefix = "alipay")
-public class AliPayConfig {
-    private String appId;
-    private String appPrivateKey;
-    private String alipayPublicKey;
-    private String notifyUrl;
-
-
-    @PostConstruct
-    public void init() {
-        // 设置参数（全局只需设置一次）
-        Config config = new Config();
-        config.protocol = "https";
-        config.gatewayHost = "openapi.alipaydev.com";
-        config.signType = "RSA2";
-        config.appId = this.appId;
-        config.merchantPrivateKey = this.appPrivateKey;
-        config.alipayPublicKey = this.alipayPublicKey;
-        config.notifyUrl = this.notifyUrl;
-        Factory.setOptions(config);
-        System.out.println("=======支付宝SDK初始化成功=======");
-    }
-}*/
 @Configuration
 @Data
 public class AlipayConfig {
